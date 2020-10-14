53bb9bb9518634afba987e2258694691ba525e13
William Moreschi <williammoreschi@gmail.com>
2020-10-14 13:55:31 -0300

    Segunda aula

---
+++ server/src/controllers/OrphanagesController.ts
@@ -0,0 +1,86 @@
+import {Request, Response} from 'express';
+import {getRepository} from 'typeorm';
+import * as Yup from 'yup';
+
+import  OrphanageView from '../views/orphanges_view';
+
+import Orphanages from '../models/Orphanage';
+
+export default {
+
+  async index(req:Request, res:Response) {
+    const orphanagesRepository = getRepository(Orphanages);
+
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    });
+
+    return res.json(OrphanageView.renderMany(orphanages));
+  },
+
+  async show(req:Request, res:Response) {
+    const { id } = req.params;
+    const orphanagesRepository = getRepository(Orphanages);
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id,{
+      relations: ['images']
+    });
+
+    return res.json(OrphanageView.render(orphanage));
+  },
+
+  async create(req:Request, res:Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    } = req.body;
+
+    const orphanagesRepository =  getRepository(Orphanages);
+
+    const reqImages = req.files as Express.Multer.File[];
+    const images = reqImages.map((image)=>{
+      return {path: image.filename}
+    });
+
+    const data = {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required('Nome Obrigatório'),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
+    });
+
+    await schema.validate(data,{
+      abortEarly: false
+    });
+
+    const orphanage = orphanagesRepository.create(data);
+
+    await orphanagesRepository.save(orphanage);
+
+    return res.status(201).json(orphanage);
+  }
+}
---
+++ server/src/models/Image.ts
@@ -0,0 +1,16 @@
+import {Entity, Column, PrimaryGeneratedColumn, ManyToMany,JoinColumn} from 'typeorm';
+import Orphanage from './Orphanage';
+@Entity('images')
+export default class Image {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  path: string;
+
+  @ManyToMany(() => Orphanage, orphanage => orphanage.images,{
+    cascade: ['insert','update']
+  })
+  @JoinColumn({name: 'orphanage_id'})
+  orphanage: Orphanage;
+}
